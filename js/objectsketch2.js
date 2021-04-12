b49c708e780d62fb36d5dfcd040f17f0e5aae01a
codalli6870 <lina532@newschool.edu>
2021-04-11 21:49:11 -0400

    Update objectsketch2.js

--- js/objectsketch2.js
+++ js/objectsketch2.js
@@ -7,7 +7,12 @@ function setup(){
 function draw(){
 	background(204);
 	ellipse(300,300,400,400);
-	ellipse(300,300,100,100);
+	ellipse(300,300,380,380);
+	ellipse(300,300,360,360);
+	ellipse(300,300,170,170);
+	
 }
 	
-	
+	fill(255);
+	beginShape();
+	vertex();
