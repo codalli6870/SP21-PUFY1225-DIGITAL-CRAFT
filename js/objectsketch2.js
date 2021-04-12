e2806b30d926ea7234b90703a4fa699bb3e2454b
codalli6870 <lina532@newschool.edu>
2021-04-11 21:30:14 -0400

    Create objectsketch2.js

---
+++ js/objectsketch2.js
@@ -0,0 +1,13 @@
+// runs once and setups the scene
+function setup(){
+	createCanvas(600, 600);
+}
+
+//runs FOREVER
+function draw(){
+	background(204);
+	ellipse(300,300,400,400);
+	ellipse(300,300,100,100);
+}
+	
+	
