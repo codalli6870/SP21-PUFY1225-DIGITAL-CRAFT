0daf32d7f4dedff6b90dc9abe277f8ea9342be19
codalli6870 <lina532@newschool.edu>
2021-04-17 23:44:54 -0400

    Update generativesketch.js
    -remove console.log

--- js/generativesketch.js
+++ js/generativesketch.js
@@ -2,7 +2,6 @@ const xPos= 36;
 const yPos= 36;
 const numRows = 7;
 const numColumns = 16;
-
 let g=70;
 
 function setup() {
@@ -12,18 +11,14 @@ function setup() {
 }
 
 function draw_ellipse(x_val, y_val) {
-  console.log("draw works");
-  console.log(x_val, y_val);
   ellipse(xPos * x_val, yPos * y_val, g);
   ellipse(xPos * x_val, yPos * y_val, g/1.5);
   ellipse(xPos * x_val, yPos * y_val, g/3);
 }
 
 function draw_sketch() {
-  console.log("drawing sketch");
   for (var row = 1; row < numRows + 1; row++) {
     for (var col = 1; col < numColumns + 1; col++) {
-      console.log("row = " + row + " , col = " + col);
       draw_ellipse(col, row);
     }
   }
