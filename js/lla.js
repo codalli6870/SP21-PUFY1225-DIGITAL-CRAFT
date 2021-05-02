89634c0f475658ca7649107e7fe2ac712cb3d06d
codalli6870 <lina532@newschool.edu>
2021-05-02 02:52:40 -0400

    Update lla.js

--- js/lla.js
+++ js/lla.js
@@ -1,31 +1,27 @@
-const wid=150
-//const variable
-
 function setup() {
   createCanvas(600, 600, SVG);
   background("black");
   noLoop();
   stroke("white");
+  colorMode(HSB);
 }
 //no repeats
 
-function draw_sketch() {
+function draw() {
   for (var i=10; i<width; i+=40) {
     strokeWeight(2);
-    line(i, y1, i+i/1.2, 80);
+    line(i, 150, i+i/1.2, 80);
     strokeWeight(1);
-    line(i, wid*2, i+i/1.2, 80);
+    line(i, 150*2, i+i/1.2, 80);
     strokeWeight(2);
-    line(i, wid*2, i+i/1.2, 600);
-    line(i, wid*2, i+i/1.4, 600);
+    line(i, 150*2, i+i/1.2, 600);
+    line(i, 150*2, i+i/1.4, 600);
   }
-}
-//creates strokes, increasing width from each other
-
-function draw () {
-  draw_sketch();
-
+  //draws strokes, increasing width from each other
   save();
+  //SVG File
 }
-//draws lines
+
+
+
 
