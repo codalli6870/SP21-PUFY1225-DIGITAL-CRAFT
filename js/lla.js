a90e2403eb41e5fde22028cfeef2a4ee9c2f3500
codalli6870 <lina532@newschool.edu>
2021-05-01 20:42:28 -0400

    Create lla.js

---
+++ js/lla.js
@@ -0,0 +1,27 @@
+const wid=150
+const widstroke=10
+let y1=0
+
+function setup() {
+  createCanvas(600, 600);
+  background("black");
+  noLoop();
+  stroke("white");
+}
+
+function draw_sketch() {
+  for (var i=10; i<width; i+=40) {
+    strokeWeight(2);
+    line(i, y1, i+i/1.2, 80);
+    strokeWeight(1);
+    line(i, wid*2, i+i/1.2, 80);
+    strokeWeight(2);
+    line(i, wid*2, i+i/1.2, 600);
+    line(i, wid*2, i+i/1.4, 600);
+  }
+}
+
+function draw () {
+  draw_sketch();
+}
+line(600, wid*2, i-i/1.5, 80);
