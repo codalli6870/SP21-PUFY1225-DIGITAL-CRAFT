fe449ea3b98a060fa79ab05cff15fccc883a1d65
codalli6870 <lina532@newschool.edu>
2021-05-01 23:17:05 -0400

    Update lla2.js

--- js/lla2.js
+++ js/lla2.js
@@ -1,12 +1,14 @@
 var numbers = [600,580,400,650,700,640,580,800,750,600,590,390]
 let g=60
 let x, y;
+//variables
 
 function setup() {
   createCanvas(600,600);
   background("black");
   noLoop();
 }
+//no repeats
 
 function draw() {
   for (var i=0; i<12; i++) {
@@ -14,6 +16,7 @@ function draw() {
     strokeWeight(random(0,40));
     fill("white");
     rect(i*50, -20, 30, numbers[i]+20);
+    //draws rectangles with random stroke weights
 
   stroke("red");
   strokeWeight(random(0,30));
@@ -21,6 +24,7 @@ function draw() {
   x = random(0, 600);
   y = random(0, 600);
   ellipse(x, y, random(30,g));
+  //draws ellipses with random stroke weights
 
   stroke("blue");
   strokeWeight(random(0,30));
@@ -28,6 +32,7 @@ function draw() {
   x = random(0, 600);
   y = random(0, 600);
   rect(x, y, random(60,100));
+  //draws rectangles with random stroke weights
 
   }
 }
